c64f06dcfef95bb47a1f677ee9835455faa7f128
Patrick Cabezas <patrick.cabezas@epn.edu.ec>
2020-06-11 09:17:08 -0500

    clase 4

--- 01-kotlin/src/Main.kt
+++ 01-kotlin/src/Main.kt
@@ -1,7 +1,7 @@
 import java.util.*
 
 fun main(args:Array<String>){
-    print("Hola")
+    println("Hola Mundo")
     //mutables
 
     //siempre usar variables inmutables.
@@ -14,20 +14,20 @@ fun main(args:Array<String>){
     var edadPersona  = 23
     //variable inmutables
     val nombreProfesor:String = "Vivente Adrian"
-    val sueldo:Float = 12.20f
+    val sueldo = 12.20
     val fechaNacimiento=Date() //new Date
     //el when es como el switch de java
     when (sueldo){
-        12.20f -> println("Sueldo normal")
-        -12.20f -> println("Sueldo negativo")
+        12.20 -> println("Sueldo normal")
+        -12.20 -> println("Sueldo negativo")
         else -> println("No se reconoce el sueldo")
     }
 
-    val esSueldoMayorAlEstalecido = (sueldo==12.20f)
+    val esSueldoMayorAlEstalecido = (sueldo==12.20)
     println(esSueldoMayorAlEstalecido)
 
     val calc=calculadorSueldo(16.00,14.00)//named parameters
-    print(calc)
+    println("el resultado de la funciones: "+calc)
 }
 //los dos valores son requeridos
 //calculoEspecial:Boolean?es un parametro opcional oyedeb se =r nulos
