d91f67dd9dc3fadeb2164379d874156e6bfa809b
Patrick Cabezas <patrick.cabezas@epn.edu.ec>
2020-06-15 09:01:00 -0500

    operadores en kotlin

--- 01-kotlin/src/Main.kt
+++ 01-kotlin/src/Main.kt
@@ -1,6 +1,6 @@
 import java.util.*
 
-fun main(args:Array<String>){
+fun main(args:Array<String>) {
     println("Hola Mundo")
     //mutables
 
@@ -28,6 +28,53 @@ fun main(args:Array<String>){
 
     val calc=calculadorSueldo(16.00,14.00)//named parameters
     println("el resultado de la funciones: "+calc)
+
+
+
+    //cualquier tipo de varialbel puede ser nula. con?
+    //arreglos
+    val arregloConstante: Array<Int> = arrayOf(1,2,3)
+    val arregloCumpleanos: ArrayList<Int> = arrayListOf<Int>(32,30,20,23)
+    //println(arregloCumpleanos)
+    arregloCumpleanos.add(24)
+    //println(arregloCumpleanos)
+    arregloCumpleanos.remove(30)
+    //print(arregloCumpleanos)
+
+    //operadores de arreglos
+    //usos de forEach
+    /*arregloCumpleanos.forEach { println("Valor de la iteracion: "+it) }
+
+    arregloCumpleanos.forEach({valorIteracion:Int ->
+        println("Valor iteracion: "+valorIteracion)
+    })
+
+    arregloCumpleanos.forEach { valorIteracion:Int->
+        println(valorIteracion)
+        //para poner variables en el string
+        println("Valor con -1 = ${valorIteracion*-1}")
+    }*/
+
+
+    //la funcion map cambiar el areglo
+    println(arregloCumpleanos)
+    val respuesta = arregloCumpleanos.map { it*-1 }
+    println(respuesta)
+
+    val respuestaMApDos= arregloCumpleanos.map { iterador:Int ->
+        val nuevoValor = iterador*-1
+        val otroValor = nuevoValor*2
+        return@map otroValor
+    }
+    println(respuestaMApDos)
+
+    val respuestaFilter=arregloCumpleanos.filter {
+        val esMayor23:Boolean=it>23
+        return@filter esMayor23
+    }
+    arregloCumpleanos.filter { iteracion:Int -> iteracion > 23 }
+    println(respuestaFilter)
+
 }
 //los dos valores son requeridos
 //calculoEspecial:Boolean?es un parametro opcional oyedeb se =r nulos
@@ -42,3 +89,5 @@ fun calculadorSueldo(
         return sueldo*tasa
     }
 }
+
+
